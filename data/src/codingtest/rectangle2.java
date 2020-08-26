9774f39139207906be762cc9ff3bc4344a097055
손현모 <lonoka2002@gmail.com>
2020-08-26 23:21:15 +0900

    2020/08/26

--- data/src/codingtest/rectangle2.java
+++ data/src/codingtest/rectangle2.java
@@ -11,6 +11,8 @@ public class rectangle2 {
 	 * 1. rectangles 2중배열에 들어있는 값 크기만큼 새로운 2중배열을 만든다
 	 * 2. 2중배열 내부에 rectangles 에 포함되는 부분만 숫자 1로 바꾼다
 	 * 3. 더한다
+	 * 4. rectangles 2중배열 내부 값은 int더라도 int*int가 int범위를 벗어날 수 있으므로
+	 * long 형의배열을 선언
 	 */
 	public static long solution(int[][] rectangles) {
 		long answer = 0;
