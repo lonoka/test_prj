04d6a74bca496753d8063109789f1749e66f7100
손현모 <lonoka2002@gmail.com>
2020-08-28 20:49:43 +0900

    2020/08/28

--- data/src/codingtest/rectangle2.java
+++ data/src/codingtest/rectangle2.java
@@ -1,5 +1,8 @@
 package codingtest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // 뭔가 잘못됨
 public class rectangle2 {
 	public static void main(String[] args) {
@@ -8,16 +11,15 @@ public class rectangle2 {
 	}
 
 	/**
-	 * 1. rectangles 2중배열에 들어있는 값 크기만큼 새로운 2중배열을 만든다
-	 * 2. 2중배열 내부에 rectangles 에 포함되는 부분만 숫자 1로 바꾼다
-	 * 3. 더한다
-	 * 4. rectangles 2중배열 내부 값은 int더라도 int*int가 int범위를 벗어날 수 있으므로
-	 * long 형의배열을 선언
+	 * 1. rectangles 2중배열에 들어있는 값 크기만큼 새로운 2중배열을 만든다 2. 2중배열 내부에 rectangles 에 포함되는
+	 * 부분만 숫자 1로 바꾼다 3. 더한다 4. rectangles 2중배열 내부 값은 int더라도 int*int가 int범위를 벗어날 수
+	 * 있으므로 long 형의배열을 선언
 	 */
 	public static long solution(int[][] rectangles) {
 		long answer = 0;
 		int x = 0;
 		int y = 0;
+
 		for (int i = 0; i < rectangles.length; i++) {
 			if (rectangles[i][2] > x) {
 				x = rectangles[i][2];
@@ -38,10 +40,29 @@ public class rectangle2 {
 
 		for (int i = 0; i < rectangle.length; i++) {
 			for (int j = 0; j < rectangle[i].length; j++) {
-				
+
 				answer += rectangle[i][j];
 			}
 		}
+		return answer;
+	}
+	/**
+	 *	HashSet을 사용하여 중복되는 값 자동 제거
+	 * 
+	 *
+	 */
+	public static long solution2(int[][] rectangles) {
+
+		Set<String> sSet = new HashSet<String>();
+		for (int i = 0; i < rectangles.length; i++) {
+			for (int j = rectangles[i][0]; j < rectangles[i][2]; j++) {
+				for (int k = rectangles[i][1]; k < rectangles[i][3]; k++) {
+					String temp = j+","+k;
+					sSet.add(temp);
+				}
+			}
+		}
+		long answer = sSet.size();
 		return answer;
 	}
 }
