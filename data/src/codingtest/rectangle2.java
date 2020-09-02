42d4ec13d851421ad689abbf24d56e1349e4d24b
손현모 <lonoka2002@gmail.com>
2020-09-02 18:36:42 +0900

    2020/09/02

--- data/src/codingtest/rectangle2.java
+++ data/src/codingtest/rectangle2.java
@@ -3,13 +3,11 @@ package codingtest;
 import java.util.HashSet;
 import java.util.Set;
 
-// 뭔가 잘못됨
+// 뭔가 잘못됨 
 public class rectangle2 {
 	public static void main(String[] args) {
-		int[][] rectangles = { { 0, 1, 4, 4 }, { 3, 1, 5, 3 } };
-		solution(rectangles);
+		
 	}
-
 	/**
 	 * 1. rectangles 2중배열에 들어있는 값 크기만큼 새로운 2중배열을 만든다 2. 2중배열 내부에 rectangles 에 포함되는
 	 * 부분만 숫자 1로 바꾼다 3. 더한다 4. rectangles 2중배열 내부 값은 int더라도 int*int가 int범위를 벗어날 수
@@ -46,11 +44,10 @@ public class rectangle2 {
 		}
 		return answer;
 	}
+
 	/**
-	 *	HashSet을 사용하여 중복되는 값 자동 제거
-	 * 	rectangles 배열 내부에서 x좌표의 시작부터 끝까지
-	 * 	y좌표의 시작부터 끝까지 각각의 좌표를 String형태로 String set에 넣음
-	 * 	넣은 set길이를 반환
+	 * HashSet을 사용하여 중복되는 값 자동 제거 rectangles 배열 내부에서 x좌표의 시작부터 끝까지 y좌표의 시작부터 끝까지 각각의
+	 * 좌표를 String형태로 String set에 넣음 넣은 set길이를 반환
 	 *
 	 */
 	public static long solution2(int[][] rectangles) {
@@ -59,7 +56,7 @@ public class rectangle2 {
 		for (int i = 0; i < rectangles.length; i++) {
 			for (int j = rectangles[i][0]; j < rectangles[i][2]; j++) {
 				for (int k = rectangles[i][1]; k < rectangles[i][3]; k++) {
-					String temp = j+","+k;
+					String temp = j + "," + k;
 					sSet.add(temp);
 				}
 			}
